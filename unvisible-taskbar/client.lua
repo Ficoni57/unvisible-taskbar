2f9263013b756388e73246cbf0150db9ba51cdcd
Ficoni <104675574+Ficoni57@users.noreply.github.com>
2022-09-18 21:46:16 +0200

    Add files via upload

---
+++ unvisible-taskbar/client.lua
@@ -0,0 +1,144 @@
+
+local lockpos = false
+local insidePrompt = false
+local guiEnabled = false
+iditema = 0
+-- openGui(length,math.random(1000000))
+function openGui(sentLength,taskID,namesent,keepWeapon)
+    if not keepWeapon then
+        TriggerEvent("actionbar:setEmptyHanded")
+    end
+    guiEnabled = true
+--    TriggerEvent('InvGliceriMrtvi')
+    SendNUIMessage({runProgress = true, Length = sentLength, Task = taskID, name = namesent})
+end
+function updateGui(sentLength,taskID,namesent)
+    SendNUIMessage({runUpdate = true, Length = sentLength, Task = taskID, name = namesent})
+end
+local activeTasks = {}
+function closeGuiFail()
+    guiEnabled = false
+    SendNUIMessage({closeFail = true})
+    --ClearPedTasks(PlayerPedId())
+end
+function closeGui()
+    guiEnabled = false
+    SendNUIMessage({closeProgress = true})
+    --ClearPedTasks(PlayerPedId())
+end
+
+function closeNormalGui()
+    guiEnabled = false
+end
+
+RegisterNUICallback('taskCancel', function(data, cb)
+  closeGui()
+  local taskIdentifier = data.tasknum
+  activeTasks[taskIdentifier] = 2
+end)
+
+RegisterNUICallback('taskEnd', function(data, cb)
+  closeNormalGui()
+  
+  local taskIdentifier = data.tasknum
+  activeTasks[taskIdentifier] = 3
+end)
+
+local taskInProcess = false
+function taskBar(length,name,runCheck,keepWeapon,vehicle,vehCheck)
+    local playerPed = PlayerPedId()
+    if taskInProcess then
+        return 0
+    end
+    if coffeetimer > 0 then
+        length = math.ceil(length * 0.66)
+    end
+    taskInProcess = true
+    local taskIdentifier = "taskid" .. math.random(1000000)
+    openGui(length,taskIdentifier,name,keepWeapon)
+    activeTasks[taskIdentifier] = 1
+    --TriggerEvent('closeInventoryGui')
+    local maxcount = GetGameTimer() + length
+    local curTime
+    while activeTasks[taskIdentifier] == 1 do
+        Citizen.Wait(1)
+        curTime = GetGameTimer()
+        if curTime > maxcount or not guiEnabled then
+            activeTasks[taskIdentifier] = 2
+        end
+        DisableControlAction(0, 289, true)
+        local fuck = 100 - (((maxcount - curTime) / length) * 100)
+        fuck = math.min(100, fuck)
+        updateGui(fuck,taskIdentifier,name)
+
+        if IsControlJustPressed(0, 200) then
+            closeGuiFail() 
+        end
+
+        if runCheck then
+            if IsPedClimbing(PlayerPedId()) or IsPedJumping(PlayerPedId()) then
+                SetPlayerControl(PlayerId(), 0, 0)
+                local totaldone = math.ceil(100 - (((maxcount - curTime) / length) * 100))
+                totaldone = math.min(100, totaldone)
+                taskInProcess = false
+                closeGuiFail()
+                Citizen.Wait(1000)
+                SetPlayerControl(PlayerId(), 1, 1)
+                Citizen.Wait(1000)
+                return totaldone
+            end
+        end
+
+        if vehicle ~= nil and vehicle ~= 0 then
+            local driverPed = GetPedInVehicleSeat(vehicle, -1)
+            if driverPed ~= playerPed and vehCheck then
+                SetPlayerControl(PlayerId(), 0, 0)
+                local totaldone = math.ceil(100 - (((maxcount - curTime) / length) * 100))
+                totaldone = math.min(100, totaldone)
+                taskInProcess = false
+                closeGuiFail()
+                Citizen.Wait(1000)
+                SetPlayerControl(PlayerId(), 1, 1)
+                Citizen.Wait(1000)
+                return totaldone
+            end
+
+            local model = GetEntityModel(playerVeh)
+            if IsThisModelACar(model) or IsThisModelABike(model) or IsThisModelAQuadbike(model) then
+                if IsEntityInAir(vehicle) then
+                    Wait(1000)
+                    if IsEntityInAir(vehicle) then
+                        local totaldone = math.ceil(100 - (((maxcount - curTime) / length) * 100))
+                        totaldone = math.min(100, totaldone)
+                        taskInProcess = false
+                        closeGuiFail()
+                        Citizen.Wait(1000)
+                        return totaldone
+                    end
+                end
+            end
+        end
+    end
+
+    local resultTask = activeTasks[taskIdentifier]
+    if resultTask == 2 then
+        local totaldone = math.ceil(100 - (((maxcount - curTime) / length) * 100))
+        totaldone = math.min(100, totaldone)
+        taskInProcess = false
+        closeGuiFail()
+        return totaldone
+    else
+        closeGui()
+        taskInProcess = false
+        EnableControlAction(0, 289, true)
+        return 100
+    end 
+   
+end
+
+function CheckCancels()
+    if IsPedRagdoll(PlayerPedId()) then
+        return true
+    end
+    return false
+end
